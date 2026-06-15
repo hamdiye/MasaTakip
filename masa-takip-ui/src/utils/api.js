be16249aa9637f9f0a6a01dff4bd2b15093688bd
hamdiye <hamdyekaya@gmail.com>
2026-06-15 16:30:44 +0300

    project moved docker container

--- masa-takip-ui/src/utils/api.js
+++ masa-takip-ui/src/utils/api.js
@@ -1,6 +1,6 @@
 import useAuthStore from '../store/useAuthStore'
 
-const BASE_URL = 'http://localhost:5115'
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5115'
 
 /**
  * Helper to fetch data from the backend API.
